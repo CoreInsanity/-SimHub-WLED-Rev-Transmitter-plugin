32dd0dc7c6d47e3b2ee18d09edb943473967216b
maxim van den eede <maximvde@gmail.com>
2020-11-19 23:51:31 +0100

    Initial commit

---
+++ Helpers/WLEDHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace SimHub.WledRevTransmitter.Helpers
+{
+    public class WLEDHelper
+    {
+        private UdpClient LedClient { get; }
+        public WLEDHelper(string ip, int port)
+        {
+            LedClient = new UdpClient(ip, port);
+        }
+        public void SendData(int red, int green, int blue, int amount)
+        {
+            var ledData = new List<Byte> { 0x01, 0x01 };
+            for (int i = 0; i < amount; i++)
+                ledData.AddRange(new List<Byte> { IntToByte(i), IntToByte(red), IntToByte(green), IntToByte(blue) });
+
+            for (int i = amount; i < 60; i++)
+                ledData.AddRange(new List<Byte> { IntToByte(i), 0x00, 0x00, 0x00 });
+
+            LedClient.Send(ledData.ToArray(), ledData.ToArray().Length);
+        }
+        private Byte IntToByte(int input)
+        {
+            return BitConverter.GetBytes(input)[0];
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,46 @@
+﻿using GameReaderCommon;
+using SimHub.Plugins;
+using System;
+using System.Diagnostics;
+using SimHub.WledRevTransmitter.Helpers;
+
+namespace SimHub.WledRevTransmitter
+{
+    [PluginDescription("A plugin to communicate with WLED")]
+    [PluginAuthor("Mushtario Pepperoni - Maxim Van den Eede")]
+    [PluginName("WLED Rev Transmitter")]
+    public class Main : IPlugin, IDataPlugin, IWPFSettings
+    {
+        public PluginManager PluginManager { get; set; }
+        private WLEDHelper LedHelper { get; set; }
+        public void DataUpdate(PluginManager pluginManager, ref GameData data)
+        {
+            if (!data.GameRunning) return;
+
+            Debug.WriteLine($"Redline: {data.NewData.MaxRpm}. RPM: {data.NewData.Rpms}");
+
+            var rpmPerLed = data.NewData.MaxRpm / 60.0;
+
+            var ledAmount = data.NewData.Rpms / rpmPerLed;
+
+            if (ledAmount >= 60) ledAmount = 59;
+            if (ledAmount < 0) ledAmount = 0;
+
+            LedHelper.SendData(255, 0, 0, Convert.ToInt32(Math.Floor(ledAmount)));
+        }
+        public void End(PluginManager pluginManager)
+        {
+
+        }
+        public System.Windows.Controls.Control GetWPFSettingsControl(PluginManager pluginManager)
+        {
+            return null;
+        }
+
+        public void Init(PluginManager pluginManager)
+        {
+            Logging.Current.Info("Starting plugin");
+            LedHelper = new WLEDHelper("192.168.0.12", 21324);
+        }
+    }
+}
